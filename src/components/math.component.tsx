2032dc27f96133477f9dd51388bbf6a0744c2896
David Morgan <ball41@gmail.com>
2021-09-11 21:44:25 +1000

    Refactor MathQuestion class

--- src/components/math.component.tsx
+++ src/components/math.component.tsx
@@ -1,43 +1,7 @@
 import { Box, Button } from "@material-ui/core";
+import { MathQuestion, Operation } from "model/mathQuestion";
 import React from "react";
 import styled from "styled-components";
-
-enum Operation {
-  ADD,
-  MINUS,
-  MULTIPLY,
-  DIVIDE,
-}
-
-class MathQuestion {
-  id: number;
-  numberOne: number;
-  numberTwo: number;
-  operation: Operation;
-
-  constructor(id: number, numberOne: number, numberTwo: number, operation?: Operation) {
-    this.id = id;
-    this.numberOne = numberOne;
-    this.numberTwo = numberTwo;
-    this.operation = operation || Operation.ADD;
-  }
-
-  getResult(): number {
-    const one = Number(this.numberOne);
-    const two = Number(this.numberTwo);
-
-    switch(this.operation) {
-      case Operation.MINUS:
-        return one - two;
-      case Operation.MULTIPLY:
-        return one * two;
-      case Operation.DIVIDE:
-        return one / two;
-      default:
-        return one + two;
-    }
-  }
-}
 
 const MathComponent = () => {
 
---
+++ src/model/mathQuestion.ts
@@ -0,0 +1,36 @@
+export enum Operation {
+  ADD,
+  MINUS,
+  MULTIPLY,
+  DIVIDE,
+}
+
+export class MathQuestion {
+  id: number;
+  numberOne: number;
+  numberTwo: number;
+  operation: Operation;
+
+  constructor(id: number, numberOne: number, numberTwo: number, operation?: Operation) {
+    this.id = id;
+    this.numberOne = numberOne;
+    this.numberTwo = numberTwo;
+    this.operation = operation || Operation.ADD;
+  }
+
+  getResult(): number {
+    const one = Number(this.numberOne);
+    const two = Number(this.numberTwo);
+
+    switch(this.operation) {
+      case Operation.MINUS:
+        return one - two;
+      case Operation.MULTIPLY:
+        return one * two;
+      case Operation.DIVIDE:
+        return one / two;
+      default:
+        return one + two;
+    }
+  }
+}
